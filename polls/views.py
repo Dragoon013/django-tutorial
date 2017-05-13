64ff39fd6d8d5e7110ab40611eddfe6f2543efc9
Jacob Teal <jacob.teal@servicerocket.com>
2017-05-13 16:26:58 -0700

    completed results form

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,9 +17,17 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request,"polls/details.html",{'question':question})
 
-def results():
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
-def vote():
-    return HttpResponse("You're voting on question %s" % question_id)
+def vote(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html',{'question':question, 'error_message':'You must make a choice.'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
